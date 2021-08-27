a63388351bd967719f9a8a85cfa2b4ee72371954
João Vitor <infamoz_br3@hotmail.com>
2021-08-26 23:29:59 -0300

    chore: funcionalidades adicionadas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) {
+      return;
+    }
+
+    const randomId = Math.floor(Math.random() * (10000 - 1)) + 1;
+
+    const newTask = {
+      id: randomId,
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const toggleTaskCompletion = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(toggleTaskCompletion);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    setTasks(tasks.filter(item => item.id !== id));
   }
 
   return (
